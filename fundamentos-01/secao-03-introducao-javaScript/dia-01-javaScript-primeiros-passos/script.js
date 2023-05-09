d0d0bb77d52eab52ccdf7eddbce5e88184ae0537
giovannalavarini <giovanalavarini.code@gmail.com>
2023-05-09 19:05:57 -0300

    Exercícios dia 01

---
+++ fundamentos-01/secao-03-introducao-javaScript/dia-01-javaScript-primeiros-passos/script.js
@@ -0,0 +1,113 @@
+// EXERCÍCIO - Condições if e else
+
+// let notaCanditada = 88;
+// if (notaCanditada >= 80) {
+//   console.log('Parabéns, você faz parte do grupo de pessoas aprovadas!');
+// }
+// else if (notaCanditada < 80 && notaCanditada >= 60) {
+//   console.log('Você está na nossa lista de espera.');
+// }
+// else {
+//   console.log('Infelizmente, você reprovou.');
+// }
+
+
+// EXERCÍCIO - Operadores lógicos
+
+// const currentHour = 15;
+// let message = '';
+// if (currentHour >= 22) {
+//   message = "Não deveríamos comer nada, é hora de dormir";
+// } else if (currentHour >= 18 && currentHour < 22) {
+//   message = "Rango da noite, vamos jantar :D";
+// } else if (currentHour >= 14 && currentHour < 18) {
+//   message = "Vamos fazer um bolo pro café da tarde?";
+// } else if (currentHour >= 11 && currentHour < 14) {
+//   message = "Hora do almoço!!!";
+// } else if (currentHour >= 4 && currentHour < 11) {
+//   message = "Hmmm, cheiro de café recém-passado";
+// }
+// console.log(message);
+
+// let weekDay = 'domingo';
+// if(weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' ||  weekDay === 'sexta-feira') {
+//   console.log('Oba, mais um dia de aprendizado na Trybe >:D”');
+// } else {
+//   console.log('FINALMENTE, descanso merecido! UwU!');
+// }
+
+
+// EXERCÍCIO - Switch e Case
+
+// let candidata = 'aprovada';
+
+// switch (candidata) {
+//   case 'aprovada':
+//     console.log('Parabéns, você está no grupo de pessoas aprovadas!');
+//     break;
+//   case 'lista':
+//       console.log('Você está na nossa lista de espera.');
+//     break;
+//   case 'reprovada':
+//       console.log('Infelizmente, você reprovou.');
+//      break;
+//   default:
+//       console.log('Informação incorreta.');
+// }
+
+// ECERCÍCIOS - Exercícios, agora, a prática
+
+let a = 60;
+let b = 10;
+let c = 20;
+
+// 01 -
+// console.log('Soma: ' + (a + b));
+// console.log('Subtração: ' + (a - b));
+// console.log('Multiplicação: ' + (a * b));
+// console.log('Divisão: ' + (a / b));
+// console.log('Módulo: ' + (a % b));
+
+// 02 -
+// if (a > b) {
+//   console.log("a maior que b");
+// } else {
+//   console.log('b maior que a');
+// }
+
+// 03 -
+// if (a > b && a > c) {
+//   console.log('a maior que b e c');
+// } else if (b > a && b > c) {
+//   console.log('b maior que a e c');
+// } else {
+//   console.log('c maior que a e b');
+// }
+
+// 04 -
+// if (a > 0) {
+//   console.log('positive');
+// } else if (a < 0) {
+//   console.log('negative');
+// } else {
+//   console.log('zero');
+// }
+
+// 05 -
+// let soma = a + b + c;
+// let anguloPositivo = a > 0 && b > 0 && c > 0;
+// Let anguloInvalido = a < 0 || b < 0 || c < 0;
+
+// if (soma === 180) {
+//   console.log('true');
+// } else if (soma !== 180) {
+//   console.log('false');
+// } else {
+//   console.log('erro');
+// }
+// AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA 
+
+// 06 -
+
+
+
