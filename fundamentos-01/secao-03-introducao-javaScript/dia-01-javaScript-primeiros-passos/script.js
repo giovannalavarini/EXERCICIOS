c2fd7e5bacd95292acd4e9d34cd950ea1569bc9b
giovannalavarini <giovanalavarini.code@gmail.com>
2023-05-10 15:55:34 -0300

    resolução exercício do dia

--- fundamentos-01/secao-03-introducao-javaScript/dia-01-javaScript-primeiros-passos/script.js
+++ fundamentos-01/secao-03-introducao-javaScript/dia-01-javaScript-primeiros-passos/script.js
@@ -57,9 +57,9 @@
 
 // ECERCÍCIOS - Exercícios, agora, a prática
 
-let a = 60;
-let b = 10;
-let c = 20;
+// let a = 60;
+// let b = 10;
+// let c = 20;
 
 // 01 -
 // console.log('Soma: ' + (a + b));
@@ -105,9 +105,82 @@ let c = 20;
 // } else {
 //   console.log('erro');
 // }
-// AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA 
+AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA 
 
 // 06 -
+// let pecaXadrez = 'bispo';
+// switch (pecaXadrez.toLowerCase()) {
+//   case 'rei':
+//     console.log('Pode ser movimentado em qualquer direção do tabuleiro, mas apenas de casa em casa.');
+//     break;
+//   case 'rainha':
+//     console.log('Sem restrições, a Rainha tem livre movimentação no jogo na horizontal, vertical e diagonais.');
+//     break;
+//   case 'torre':
+//     console.log('pode correr sem restrição de número de casas e em todas as direções (frente, trás, direita, esquerda).');
+//     break;
+//   case 'bispo':
+//     console.log('Sem restrição de número de casas, mas somente no sentido diagonal.');
+//     break;
+//   case 'cavalo':
+//     console.log('Realiza movimentos em “L”, ou seja, duas casas em um sentido e uma casa em sentido perpendicular àquele, em qualquer direção.');
+//     break;
+//   case 'peão':
+//     console.log('Pode apenas realizar movimentos frontais, de forma que o primeiro movimento de cada peão abranja até duas casas, e os demais se limitam a uma casa à frente. O ataque do peão sempre ocorre na diagonal.');
+//     break;
+//     default:
+//       console.log('erro');
+// }
+
+// 07 -
+
+// let nota = 80;
+// if (nota < 0 || nota > 100){
+//   console.log('erro');
+// } else if (nota >= 90) {
+//   console.log('Sua nota é A');
+// } else if (nota >= 80) {
+//   console.log('Sua nota é B');
+// } else if (nota >= 70) {
+//   console.log('Sua nota é C');
+// } else if (nota >= 60) {
+//   console.log('Sua nota é D');
+// } else if (nota >= 50) {
+//   console.log('Sua nota é E');
+// } else {
+//   console.log('Sua nota é E');
+// }
+
+// 08 -
+// let a = 10;
+// let b = 8;
+// let c = 2;
+
+// let impar = false;
+
+// if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+//   impar = true;
+// }
+// console.log(impar);
+
+// 09 -
+// let par = false;
+
+// if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
+//   par = true;
+// }
+// console.log(par);
+
+// 10 -
+// let valorCusto = 10;
+// let valorVenda = 20;
+
+// if 
+AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA 
+
+// 11 -
+AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA AJUDA 
+
 
 
 
