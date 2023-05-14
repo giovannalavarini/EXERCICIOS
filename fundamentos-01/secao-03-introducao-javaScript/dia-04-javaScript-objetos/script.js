e859716028061b4564815d9b8e5eb77ad6a4f5a3
giovannalavarini <giovanalavarini.code@gmail.com>
2023-05-14 00:33:06 -0300

    Exercícios do projeto Playground Functions

---
+++ fundamentos-01/secao-03-introducao-javaScript/dia-04-javaScript-objetos/script.js
@@ -0,0 +1,52 @@
+// // 1 - Crie uma função usando o operador &&
+
+// const girafa = true;
+// const elefante = true;
+// const macaco = false;
+
+// function compareTrue(animal1, animal2) {
+//   if (animal1 === true && animal2 === true) {
+//     return true
+//   } else if (animal1 === false || animal2 === false) {
+//     return false
+//   } else {
+//     return false
+//   }
+// }
+// console.log(compareTrue(girafa,elefante));
+// console.log(compareTrue(macaco,elefante));
+
+// // 2 - Crie uma função que divida uma frase
+
+// let frase = 'go Trybe';
+
+// function splitSentence(frase) {
+//   return frase.split(' ')
+// }
+// console.log(splitSentence('go Trybe'));
+
+// // 3 - Crie uma função que use concatenação de strings
+
+// function concatName(array) {
+//   return array[array.length - 1] + ', ' + array[0]
+// }
+
+// let nomes = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
+// console.log(concatName(nomes));
+
+// let frase1 = ['foguete', 'não', 'tem', 'ré'];
+// console.log(concatName(frase1));
+
+// let frase2 = ['captain', 'my', 'captain'];
+// console.log(concatName(frase2));
+
+// // 4 - Crie uma função que calcula a quantidade de pontos em um campeonato de futebol
+
+// function footballPoints(wins, ties) {
+//   return (wins * 3) + ties
+// }
+
+// console.log(footballPoints(14, 8));
+// console.log(footballPoints(1, 2));
+// console.log(footballPoints(0, 0));
+
