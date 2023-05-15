445e1d86f2f3e5eaaa15937cd3479397c684094c
giovannalavarini <giovanalavarini.code@gmail.com>
2023-05-15 09:18:34 -0300

    Exercícios dia 3.4

--- fundamentos-01/secao-03-introducao-javaScript/dia-04-javaScript-objetos/script.js
+++ fundamentos-01/secao-03-introducao-javaScript/dia-04-javaScript-objetos/script.js
@@ -40,13 +40,63 @@
 // let frase2 = ['captain', 'my', 'captain'];
 // console.log(concatName(frase2));
 
-// // 4 - Crie uma função que calcula a quantidade de pontos em um campeonato de futebol
+// // // 4 - Crie uma função que calcula a quantidade de pontos em um campeonato de futebol
 
-// function footballPoints(wins, ties) {
-//   return (wins * 3) + ties
+// // function footballPoints(wins, ties) {
+// //   return (wins * 3) + ties
+// // }
+
+// // console.log(footballPoints(14, 8));
+// // console.log(footballPoints(1, 2));
+// // console.log(footballPoints(0, 0));
+
+// // EXERCÍCIOS - Objetos
+
+// let player = {
+//  name: 'Marta',
+//  lastName: 'Silva',
+//  age: 34,
+//  medals: { 
+//   golden: 2, 
+//   silver: 3 },
+// };
+
+// player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+
+// console.log('A jogadora ' + player.name + ' ' + player.lastName + 'tem ' + player.age + ' anos de idade.');
+
+// console.log('A jogadora ' + player['name'] + ' ' + player['lastName'] + ' foi eleita a melhor do mundo por ' + player['bestInTheWorld'].length + ' vezes.');
+
+// console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
+
+//  EXERCÍCIOS - For/in e for/of
+
+// For/in -
+
+// let cars = ['Saab', 'Volvo', 'BMW'];
+
+// for (let index in cars) {
+//   console.log(cars[index]);
 // }
 
-// console.log(footballPoints(14, 8));
-// console.log(footballPoints(1, 2));
-// console.log(footballPoints(0, 0));
+// let car = {
+//   type: 'Fiat',
+//   model: '500',
+//   color: 'white',
+// };
 
+// let food = ['hambúrguer', 'bife', 'acarajé'];
+// for (let index in car) {
+//   console.log(index, car[index]);
+// }
+//resultado: 0, 1, 2;
+
+// for/of - 
+
+// let food = ['hambúrguer', 'bife', 'acarajé'];
+// for (let value of food) {
+//   console.log(value);
+// };
+// //resultado: hambúrguer, bife, acarajé;
+
+// for/in - 
