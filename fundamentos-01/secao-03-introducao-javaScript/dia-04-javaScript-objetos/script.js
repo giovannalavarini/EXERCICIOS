a05e04ee9948780510de6941cded505e83ab3abb
giovannalavarini <giovanalavarini.code@gmail.com>
2023-05-16 16:41:07 -0300

    Exercícios 3.4

--- fundamentos-01/secao-03-introducao-javaScript/dia-04-javaScript-objetos/script.js
+++ fundamentos-01/secao-03-introducao-javaScript/dia-04-javaScript-objetos/script.js
@@ -6,49 +6,84 @@
 
 // function compareTrue(animal1, animal2) {
 //   if (animal1 === true && animal2 === true) {
-//     return true
-//   } else if (animal1 === false || animal2 === false) {
-//     return false
-//   } else {
-//     return false
-//   }
-// }
-// console.log(compareTrue(girafa,elefante));
-// console.log(compareTrue(macaco,elefante));
+// //     return true
+// //   } else if (animal1 === false || animal2 === false) {
+// //     return false
+// //   } else {
+// //     return false
+// //   }
+// // }
+// // console.log(compareTrue(girafa,elefante));
+// // console.log(compareTrue(macaco,elefante));
 
-// // 2 - Crie uma função que divida uma frase
+// // // 2 - Crie uma função que divida uma frase
 
-// let frase = 'go Trybe';
+// // let frase = 'go Trybe';
 
-// function splitSentence(frase) {
-//   return frase.split(' ')
-// }
-// console.log(splitSentence('go Trybe'));
+// // function splitSentence(frase) {
+// //   return frase.split(' ')
+// // }
+// // console.log(splitSentence('go Trybe'));
 
-// // 3 - Crie uma função que use concatenação de strings
+// // // 3 - Crie uma função que use concatenação de strings
 
-// function concatName(array) {
-//   return array[array.length - 1] + ', ' + array[0]
-// }
-
-// let nomes = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
-// console.log(concatName(nomes));
-
-// let frase1 = ['foguete', 'não', 'tem', 'ré'];
-// console.log(concatName(frase1));
-
-// let frase2 = ['captain', 'my', 'captain'];
-// console.log(concatName(frase2));
-
-// // // 4 - Crie uma função que calcula a quantidade de pontos em um campeonato de futebol
-
-// // function footballPoints(wins, ties) {
-// //   return (wins * 3) + ties
+// // function concatName(array) {
+// //   return array[array.length - 1] + ', ' + array[0]
 // // }
 
-// // console.log(footballPoints(14, 8));
-// // console.log(footballPoints(1, 2));
-// // console.log(footballPoints(0, 0));
+// // let nomes = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
+// // console.log(concatName(nomes));
+
+// // let frase1 = ['foguete', 'não', 'tem', 'ré'];
+// // console.log(concatName(frase1));
+
+// // let frase2 = ['captain', 'my', 'captain'];
+// // console.log(concatName(frase2));
+
+// // // // 4 - Crie uma função que calcula a quantidade de pontos em um campeonato de futebol
+
+// // // function footballPoints(wins, ties) {
+// // //   return (wins * 3) + ties
+// // // }
+
+// // // console.log(footballPoints(14, 8));
+// // // console.log(footballPoints(1, 2));
+// // // console.log(footballPoints(0, 0));
+
+// // 5 - Crie uma função que calcula o número de repetições do maior número
+
+// function highestCount(numeros) {
+//   let counter = 0;
+//   let maiorNumero = numeros [0];
+  
+//   for (let index = 0; index < numeros.length; index += 1) {
+//     if (numeros[index] === maiorNumero){
+//       counter += 1;
+//     }
+    
+//   }
+//   return counter;
+// }
+// console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
+// console.log(highestCount([0, 4, 4, 4, 9, 2, 1]));
+// console.log(highestCount([0, 0, 0]));
+
+// Crie 3 funções para calcular as áreas de um triângulo e de um retângulo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // // EXERCÍCIOS - Objetos
 
@@ -100,3 +135,65 @@
 // //resultado: hambúrguer, bife, acarajé;
 
 // for/in - 
+
+// 1 - 
+// let names = {
+//   person1: 'João',
+//   person2: 'Maria',
+//   person3: 'Jorge',
+// };
+
+// for (const key in names) {
+//   console.log('Olá, ' + names[key]); 
+// }
+
+// 2 -
+// let car = {
+//   model: 'A3 Sedan',
+//   manufacturer: 'Audi',
+//   year: 2020
+// };
+
+// for (const key in car) {
+//   console.log(key + ': ' + car[key]);
+// }
+
+// Exercícios -  Adicionando novas chaves
+
+// let student = {}
+
+// function addProperty (object, key, value) {
+// //   object[key] = value;
+// // };
+
+// // addProperty(student, 'nome', 'Giovanna Lavarini');
+// // addProperty(student, 'e-mail', 'giovanna.lavarini@gmail.com');
+// // addProperty(student, 'telefone', '(31) 99502-8076');
+// // console.log(student);
+
+// // Exercícios - Object.entries
+
+// let countries = {
+//   franca: 'Paris',
+//   brasil: 'Brasília',
+//   espanha: 'Madrid',
+//   portugal: 'Lisboa',
+// };
+// let pairKeyValue = Object.entries(countries);
+// console.log(pairKeyValue);
+
+// for(index in pairKeyValue) {
+//   console.log('--------');
+//   console.log('País:', pairKeyValue[index][0]);
+//   console.log('Capital:', pairKeyValue[index][1]);
+// };
+
+//  Exercícios - var, let e const
+
+if (true) {
+  // inicio do escopo do if
+  const userAge = '20';
+  console.log(userAge); // 20
+  // fim do escopo do if
+}
+console.log(userAge); // 20
